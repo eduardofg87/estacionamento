bd8eb012d5601416bfc6f7f95457ea23839f42d3
Eduardo Figueiredo Gonçalves <goncalves.ef@gmail.com>
2023-11-29 20:17:00 -0300

    models finished,  controller started, business finished

--- api/api/Controllers/EstacionamentoController.cs
+++ api/api/Controllers/EstacionamentoController.cs
@@ -4,16 +4,16 @@ namespace api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class EstacionamentoController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<EstacionamentoController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public EstacionamentoController(ILogger<EstacionamentoController> logger)
     {
         _logger = logger;
     }
